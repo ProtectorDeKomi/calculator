c1709d3da57a0a3a7a784a40af00aa6216fe7914
Astartes <97326719+ProtectorDeKomi@users.noreply.github.com>
2022-12-22 20:28:06 -0600

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,29 +1,75 @@
-def main():
-    print("Suma : +\nResta : -\nMultiplicacion : x\nDivicion : /\nPorcentaje : %\nPotencia : **\n\n")
-    val1 = int(input("Valor Primario :"))
-    var = input("Operacion :")
-    val2 = int(input("Valor secundario :"))
+import sys
+
+def manual():
+    """"
+    Calculadora En Terminal
+    Ejemplo De Uso:
     
-    if "x" in var:
-        echo = val1 * val2
-        print(echo)
+    Suma
+    Valor Primario : 1
+    Operacion : +
+    Valor Secundario : 3
+
+    Resta
+    Valor Primario : 6
+    Operacion : -
+    Valor Secundario : 2
+
+    Multiplicacion
+    Valor Primario : 10
+    Operacion : x
+    Valor Secundario : 4
+
+    Divicion
+    Valor Primario : 1
+    Operacion : /
+    Valor Secundario : 3
+
+    Porcentaje
+    Valor Primario : 10
+    Operacion : %
+    Valor Secundario : 3
+
+    Potencias
+    Valor Primario : 1
+    Operacion : **
+    Valor Secundario : 3
+    """
+    help.__doc__
+
+def brain(num1,oper,num2):
+
+    operation = ['+', '-', 'x','/','*','%']
+    if oper in operation:
+        if oper == '+':
+            print(num1 + num2)
+        elif oper == '-':
+            print(num1 - num2)
+        elif oper == 'x':
+            print(num1 * num2)
+        elif oper == '/':
+            print(num1 / num2)
+        elif oper == '%':
+            print(num1 % num2)
+
     else:
-        if "+" in var:
-            echo = val1 + val2
-            print(echo)
-        else:
-            if "-" in var:
-                echo = val1 - val2
-                print(echo)
-            else:
-                if "/" in var:
-                  echo = val1 // val2
-                  print(echo)  
-                else:
-                    if "**" in var:
-                        echo = val1 ** val2
-                        print(echo) 
+        print(f"La Opcion '{oper}' No Es Valida")
 
+if __name__ == "__main__":
+    option = int(input("""
+    Calculadora Basica En Terminal By Astartes
 
-        
-main()
+    1) Usar Calculadora
+    2) Ayuda
+    3) Salir
+
+    Opcion : """))
+    if option == 3:
+        sys.exit()
+    elif option == 1:
+        val1 = float(input("Valor Primario :"))
+        var = input("Operacion :")
+        val2 = float(input("Valor secundario :"))
+        brain(val1, var,val2)
+    elif option == 2:
+        help(manual)
